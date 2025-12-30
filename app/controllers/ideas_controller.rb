b2d5b36230c54b10da243027c92e02791b65a48c
sarun dhoju <sarundhoju775@gmail.com>
2025-12-30 11:00:22 +0000

    consent form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,15 @@
 class IdeasController < ApplicationController
-    def index
-        @ideas = Idea.all
-    end
+  def index
+    @ideas = Idea.all
+  end
+
+  def create
+    @idea = Idea.create(idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:description, :author)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root "ideas#index"
+    resources :ideas
+    resources :controller_name
 end
