85cc6bbfefe21ec7472bb8001f953840d9f7ffc0
sarun dhoju <sarundhoju775@gmail.com>
2026-01-07 01:21:57 +0000

    chapter 5 continued to the form

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -9,6 +9,18 @@ class IdeasController < ApplicationController
   end
 
   private
+  def edit
+  @idea = Idea.find(params[:id])
+end
+def create
+  @idea = Idea.create(idea_params)
+  if @idea.valid?
+    # Implement later
+  else
+    # Implement later
+  end
+  redirect_to root_path
+end
 
   def idea_params
     params.require(:idea).permit(:description, :author)
